0177e8b02a7da2478c58b124929df82fc1acc4ad
Paul-Cristian Vasile <paul.cristian.vasile04@gmail.com>
2020-11-03 12:06:52 +0200

    Initial Commit

---
+++ Home Management/Home_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_ManagementApp.swift
+//  Home Management
+//
+//  Created by Paul Vasile on 03/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Home_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
